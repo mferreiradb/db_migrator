a5ce51912ccdfa7ba9bd3b55ca1fe63177815c35
mferreiradb <mauhmg@gmail.com>
2023-04-12 12:56:53 -0300

    Update

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const db = require('mysql2');
 const path = require('path');
 
 const arquivoExcel = path.resolve(__dirname, 'test.xlsx')
+//console.log(arquivoExcel);
 
-const buffer = fs.readFileSync(arquivoExcel, 'utf-8')
+const buffer = fs.readFileSync(arquivoExcel)
+//console.log(buffer);
 
 const workbook = xlsx.read(buffer)
+//console.log(workbook);
+
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 //const worksheet = workbook.Sheets['test']
+console.log(worksheet['A1']);
 
 const data = xlsx.utils.sheet_to_json(worksheet)
 
