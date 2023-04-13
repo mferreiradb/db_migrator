3d4a781b8464905c3d6e25b0d21d43c3a38b4ef8
Mauricio <mauhmg@gmail.com>
2023-04-13 14:57:46 -0300

    Added environment variables

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const db = require('mysql2');
+const env = require('dotenv').config();
+
+console.log(env.USER_DB);
 
 const connection = db.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'erp_lion'
+    host: env.HOSTNAME,
+    user: env.USER_DB,
+    password: env.USER_PASSWORD,
+    database: env.DB_NAME
 });
 
 console.log('Database connected.');
