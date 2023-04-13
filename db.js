e671d17a5338455bb323b56a03030488a2281a7a
Mauricio <mauhmg@gmail.com>
2023-04-13 15:47:25 -0300

    Add EsLint

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const db = require('mysql2');
-require('dotenv').config()
+require('dotenv').config();
 
 const connection = db.createConnection({
-    host: process.env.HOSTNAME,
-    user: process.env.USER_DB,
-    password: process.env.USER_PASSWORD,
-    database: process.env.DB_NAME
+	host: process.env.HOSTNAME,
+	user: process.env.USER_DB,
+	password: process.env.USER_PASSWORD,
+	database: process.env.DB_NAME
 });
 
 console.log('Database connected.');
--- index.js
+++ index.js
@@ -3,32 +3,32 @@ const xlsx = require('xlsx');
 const path = require('path');
 const connection = require('./db');
 
-const arquivoExcel = path.resolve(__dirname, 'migrador.xlsx')
-const buffer = fs.readFileSync(arquivoExcel)
-const workbook = xlsx.read(buffer)
+const arquivoExcel = path.resolve(__dirname, 'migrador.xlsx');
+const buffer = fs.readFileSync(arquivoExcel);
+const workbook = xlsx.read(buffer);
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-const data = xlsx.utils.sheet_to_json(worksheet)
+const data = xlsx.utils.sheet_to_json(worksheet);
 
 for (const row of data) {
 
-    //EXCEL DATAS
-    const { DATA_EMISSAO, DATA_VENCIMENTO, DATA_PAGAMENTO, IDPESSOA, NRO_PARCELA, VALOR_JUROS, VALOR_MULTA, VALOR_DESCONTO, VALOR_TOTAL, VALOR_CONTA, HISTORICO, IDSTATUS, VALOR_PAGO, NUM_DOC } = row
+	//EXCEL DATAS
+	const { DATA_EMISSAO, DATA_VENCIMENTO, DATA_PAGAMENTO, IDPESSOA, NRO_PARCELA, VALOR_JUROS, VALOR_MULTA, VALOR_DESCONTO, VALOR_TOTAL, VALOR_CONTA, HISTORICO, IDSTATUS, VALOR_PAGO, NUM_DOC } = row;
 
-    //TRANSFORM STRINGS IN DATE
-    const data_emissao_formatada = new Date(DATA_EMISSAO)
-    const data_vencimento_formatada = new Date(DATA_VENCIMENTO)
-    const data_pagamento_formatada = new Date(DATA_PAGAMENTO)
+	//TRANSFORM STRINGS IN DATE
+	const data_emissao_formatada = new Date(DATA_EMISSAO);
+	const data_vencimento_formatada = new Date(DATA_VENCIMENTO);
+	const data_pagamento_formatada = new Date(DATA_PAGAMENTO);
 
-    //TRANSFORM DATES IN STRING
-    const DATA_EMISSAO_FORMATADA = data_emissao_formatada.toISOString().slice(0, 10)
-    const DATA_VENCIMENTO_FORMATADA = data_vencimento_formatada.toISOString().slice(0, 10)
-    const DATA_PAGAMENTO_FORMATADA = data_pagamento_formatada.toISOString().slice(0, 10)
+	//TRANSFORM DATES IN STRING
+	const DATA_EMISSAO_FORMATADA = data_emissao_formatada.toISOString().slice(0, 10);
+	const DATA_VENCIMENTO_FORMATADA = data_vencimento_formatada.toISOString().slice(0, 10);
+	const DATA_PAGAMENTO_FORMATADA = data_pagamento_formatada.toISOString().slice(0, 10);
 
-    const sql = 'insert into contas_receber (DATA_EMISSAO, DATA_VENCIMENTO, DATA_PAGAMENTO, IDPESSOA, NRO_PARCELA, VALOR_JUROS, VALOR_MULTA, VALOR_DESCONTO, VALOR_TOTAL, VALOR_CONTA, HISTORICO, IDSTATUS, VALOR_PAGO, NUM_DOC) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+	const sql = 'insert into contas_receber (DATA_EMISSAO, DATA_VENCIMENTO, DATA_PAGAMENTO, IDPESSOA, NRO_PARCELA, VALOR_JUROS, VALOR_MULTA, VALOR_DESCONTO, VALOR_TOTAL, VALOR_CONTA, HISTORICO, IDSTATUS, VALOR_PAGO, NUM_DOC) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
 
-    connection.execute(sql, [DATA_EMISSAO_FORMATADA, DATA_VENCIMENTO_FORMATADA, DATA_PAGAMENTO_FORMATADA, IDPESSOA, NRO_PARCELA, VALOR_JUROS, VALOR_MULTA, VALOR_DESCONTO, VALOR_TOTAL, VALOR_CONTA, HISTORICO, IDSTATUS, VALOR_PAGO, NUM_DOC]);
+	connection.execute(sql, [DATA_EMISSAO_FORMATADA, DATA_VENCIMENTO_FORMATADA, DATA_PAGAMENTO_FORMATADA, IDPESSOA, NRO_PARCELA, VALOR_JUROS, VALOR_MULTA, VALOR_DESCONTO, VALOR_TOTAL, VALOR_CONTA, HISTORICO, IDSTATUS, VALOR_PAGO, NUM_DOC]);
 }
 
-connection.end()
+connection.end();
 
-console.log(`Migration successful. ${data.length} lines inserted.`)
+console.log(`Migration successful. ${data.length} lines inserted.`);
